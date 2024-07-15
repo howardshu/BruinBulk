8d3b398b56e4e4a74fb8a21e0c2c7a82d3177cff
howardshu <63481712+howardshu@users.noreply.github.com>
2024-07-14 21:21:01 -0700

    Initial Commit

---
+++ Bruin Bulk/Bruin_BulkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bruin_BulkApp.swift
+//  Bruin Bulk
+//
+//  Created by Howard on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Bruin_BulkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bruin Bulk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bruin Bulk
+//
+//  Created by Howard on 7/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
