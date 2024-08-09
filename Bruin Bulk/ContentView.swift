4b4ceb01e357c2dd30acf33a3de6925ad45f2b60
howardshu <63481712+howardshu@users.noreply.github.com>
2024-08-08 19:08:05 -0700

    Start GitHub + commit past home page UI initial design

--- Bruin Bulk/ContentView.swift
+++ Bruin Bulk/ContentView.swift
@@ -10,12 +10,50 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("Bruin Bulk")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }.padding().background(.blue)
+                
+            Spacer()
+            
+            HStack(alignment: .lastTextBaseline) {
+                Spacer()
+                VStack {
+                    Image(systemName: "house")
+                    Text("Home")
+                        .font(.caption)
+                        .fontWeight(.regular)
+                }
+                Spacer()
+                
+                VStack {
+                    Image(systemName: "fork.knife")
+                    Text("Log Food")
+                        .font(.caption)
+                        .fontWeight(.regular)
+                }
+                Spacer()
+                
+                VStack {
+                    Image(systemName: "calendar")
+                    Text("Plan")
+                        .font(.caption)
+                        .fontWeight(.regular)
+                }
+                Spacer()
+                
+                VStack {
+                    Image(systemName: "ellipsis.circle")
+                    Text("More")
+                        .font(.caption)
+                        .fontWeight(.regular)
+                }
+                Spacer()
+            }.padding().background(.yellow)
         }
-        .padding()
     }
 }
 
